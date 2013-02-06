118da82e7b02ab53cdee820baed2f1f9bee619e1
OpenShift guest <johan.leitet@lnu.se>
2013-02-06 09:38:54 -0500

    Beginning to get the pieces in there...

--- script/app.js
+++ script/app.js
@@ -1,17 +1,59 @@
 (function(){
-	
-	var Note = Backbone.Model.extend({
-		text : function(){
-			var text = "Note text";
-			this.get({text: text});
-			return text;
-		}
+    
+	var Student = Backbone.Model.extend({
+		name : function() { return this.get('name'); }
 
 	});
+    
+    var StudentView = Backbone.View.extend({
+    
+        template: _.template($('#student-template').html()),
+        
+        tagName: "p",
+    
+        className: "student-view",
+    
+        events: {
+     
+        },
+    
+        initialize: function() {
+            this.listenTo(this.model, "change", this.render);   // alt: this.model.on('change', this.render, this);
+        
+        },
+    
+        render: function() {
+            console.log(this.template(this.model.attributes));
+        }
+    
+    });
+    
+    var Router = Backbone.Router.extend({
+        routes: {
+            "":                     "index",
+            "students":             "students",
+            "students/:username":   "student"
+        },
+        index: function()               { console.log("on index");},
+        students: function()            { console.log("on students"); },
+        student: function(username)     { console.log("on one student"); },
+        
+    });
 
-	var myNote = new Note();
+    var app = new Router();
+    
+    Backbone.history.start()
+ 
+    app.navigate("students", {trigger:true});
+    app.navigate("students/tstjo", {trigger:true});
 
-	alert(myNote.text());
+    var myStudent = new Student();
 
+
+    var studentView = new StudentView({model: myStudent});
+
+    myStudent.set({'name': "test"});
+    
+    myStudent.set({'name' : "Ellen Nu"});
 	
 })();
